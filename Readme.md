ec725a0480d3bc819e864aadd5554af58de79f72
ronaldo <ronaldo.dsantos@gmail.com>
2024-09-28 22:10:25 -0300

    Estrutura de Controle

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## Material de estudos sobre estrutura de controle no C#
--- obj/Debug/net8.0/Estrutura-de-Controle.AssemblyInfo.cs
+++ obj/Debug/net8.0/Estrutura-de-Controle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estrutura-de-Controle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ecb8ced97627f57a36652d6963b8fe5cdd52636")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estrutura-de-Controle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estrutura-de-Controle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
